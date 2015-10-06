31460bae01f122c9bae47f66de364c61696a5ae8
noobiehacker <davidkwokhochan@gmail.com>
2015-10-06 00:04:52 -0700

    Added login assets to login IB

---
+++ Homie/Stores/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Homie
+//
+//  Created by davix on 10/5/15.
+//  Copyright © 2015 Homie. All rights reserved.
+//
+
+import Foundation
+
+struct Config{
+    
+    
+}
+
+struct Color{
+    
+    
+}
