e56c61b36d0af6bcb5b173afc887bec0d34567d2
noobiehacker <davidkwokhochan@gmail.com>
2015-10-07 15:39:54 -0700

    Added alert box and Community View Controller

--- Homie/Controllers/CommunityViewController.swift
+++ Homie/Controllers/CommunityViewController.swift
@@ -1,29 +1,25 @@
 //
-//  ViewController.swift
+//  CommunityViewController.swift
 //  Homie
 //
-//  Created by davix on 10/3/15.
+//  Created by davix on 10/7/15.
 //  Copyright © 2015 Homie. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class CommunityViewController: UIViewController {
+    
+    var backEndManager : BackEndProtocol = MockBackEndManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        MockBackendData()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func loginButtonPressed(){
-        
-        print("hello world")
-    }
-}
 
+}
