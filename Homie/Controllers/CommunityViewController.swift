1950ebfe8f7a63dbebcb08f2c44f638d3ae1636d
noobiehacker <davidkwokhochan@gmail.com>
2015-10-08 19:49:21 -0700

    Added Community interface builder and fixed home login page contraints

--- Homie/Controllers/CommunityViewController.swift
+++ Homie/Controllers/CommunityViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
 class CommunityViewController: UIViewController {
     
     var backEndManager : BackEndProtocol = MockBackEndManager()
+
+    @IBOutlet var mapViewOutlet: MKMapView!
     
+    @IBAction func DinningButtonPressed(sender: AnyObject) {
+        print("dinning button pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
