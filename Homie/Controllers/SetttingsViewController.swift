17c8aba844e01fa3a3dd9c65407a4115c8e43112
noobiehacker <davidkwokhochan@gmail.com>
2015-10-13 18:41:25 -0700

    Added Splash Screen and App Icon

---
+++ Homie/Controllers/SetttingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SetttingsController.swift
+//  Homie
+//
+//  Created by davix on 10/13/15.
+//  Copyright © 2015 Homie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+}
